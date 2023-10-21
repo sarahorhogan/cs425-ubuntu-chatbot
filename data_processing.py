7237b051c5bfda8eac26791eddcddac431176dfb
SARAH ANN HOGAN _ <sahogan.2020@smu.edu.sg>
2023-10-21 09:23:19 +0800

    Added script to process dialogue dataset

---
+++ data_processing.py
@@ -0,0 +1,58 @@
+import csv
+
+# Define a dictionary to store dialogue-based conversation units
+dialogues = {}
+
+# Read the CSV file
+with open('Ubuntu-dialogue-corpus/dialogueText.csv', mode='r', encoding='utf-8') as file:
+    csv_reader = csv.reader(file)
+    
+    current_dialogue = None
+    current_turn = 0 
+    person_asking = None
+
+    for row in csv_reader:
+        folder, dialogueID, date, sender, receiver, text = row
+        
+        if dialogueID not in dialogues:
+            dialogues[dialogueID] = {'questions': [], 'answers': []}
+            person_asking = sender 
+            current_turn = 0 
+            current_dialogue = dialogueID 
+        
+        # Check if the current sender is the person asking 
+        if sender == person_asking:
+            if len(dialogues[dialogueID]['questions']) == 0:
+                dialogues[dialogueID]['questions'].append(text)
+            elif (len(dialogues[dialogueID]['questions']) >= current_turn + 1 and len(dialogues[dialogueID]['answers']) >= current_turn + 1):
+                current_turn += 1 
+                dialogues[dialogueID]['questions'].append(text)
+            #Append to the previous question if no answer yet 
+            else:
+                dialogues[dialogueID]['questions'][current_turn] += ' ' + text
+        else:
+            # Append to the question
+            dialogues[dialogueID]['answers'].append(text)
+        
+
+output_file = 'dialogue_pairs.csv'
+
+# Write the dialogue pairs to a CSV file
+with open(output_file, mode='w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    
+    # Write the header row
+    writer.writerow(['DialogueID', 'Question', 'Answer'])
+    
+    # Write the dialogue-based question-answer pairs
+    for dialogueID, dialog_data in dialogues.items():
+        for question, answer in zip(dialog_data['questions'], dialog_data['answers']):
+            writer.writerow([dialogueID, question, answer])
+
+print(f'Dialogue pairs saved to {output_file}')
+
+
+
+
+
+
